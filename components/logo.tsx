0804f5974d72159d45b203829df83b1e68062396
imranKhanYRT <imran.khan.yrt@gmail.com>
2024-03-31 01:40:27 +0600

    add navbar

--- components/logo.tsx
+++ components/logo.tsx
@@ -12,7 +12,7 @@ export const Logo = () => {
 	return (
 		<Link href="/">
 			<div className="hover:opacity-75 transition items-center gap-x-3 hidden md:flex">
-				<Image src="/logo.png" alt="Logo" height={30} width={30} />
+				<Image src="/logo.svg" alt="Logo" height={30} width={30} />
 				<p className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}>Taskify</p>
 			</div>
 		</Link>
