a3a6bcbc3c862093c0157aec24fab4af17c1152c
imranKhanYRT <imran.khan.yrt@gmail.com>
2024-03-31 11:21:22 +0600

    add footer fix

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -3,11 +3,11 @@ import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
 	return (
-		<div className="fixed bottom-0 w-full px-4 border-t bg-slate-100">
+		<div className="fixed bottom-0 w-full p-4 border-t bg-slate-100">
 			{/* specific width control of children - weird way to not allow the logo visible */}
 			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
 				<Logo />
-				<div className="md:max-w-screen-2xl w-full space-x-4 flex justify-between items-center">
+				<div className="md:w-auto w-full space-x-4 flex justify-between items-center">
 					<Button size="sm" variant="ghost">
 						Privacy Policy
 					</Button>
