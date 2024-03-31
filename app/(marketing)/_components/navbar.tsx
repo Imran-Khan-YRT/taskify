1f747ed55c173836f5d0dea159555f8a6dd973f2
imranKhanYRT <imran.khan.yrt@gmail.com>
2024-03-31 11:18:46 +0600

    add footer

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,14 +5,13 @@ import Link from "next/link";
 export const Navbar = () => {
 	return (
 		<div className="fixed top-0 w-full h-14 px-4 border-b-shadow-sm bg-white flex items-center">
-			{/* specific width control of children - weird way to not allow the logo visible */}
 			<div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
 				<Logo />
-				<div className="md:max-w-screen-2xl w-full space-x-4 flex justify-between items-center">
+				<div className="space-x-4 md:w-auto w-full flex items-center justify-between">
 					<Button size="sm" variant="outline" asChild>
 						<Link href="/sign-in">Login</Link>
 					</Button>
-					<Button asChild>
+					<Button size="sm" asChild>
 						<Link href="/sign-up">Get Taskify For Free</Link>
 					</Button>
 				</div>
